bd191929755ecd281d3ec1cea54e2699820c2ad2
Aman Bansal <aman210697@gmail.com>
2023-01-30 17:20:00 +0530

    use base62 to encode

--- src/main/kotlin/url/shortener/service/BaseConversion.kt
+++ src/main/kotlin/url/shortener/service/BaseConversion.kt
@@ -1,4 +1,20 @@
 package url.shortener.service
 
 class BaseConversion {
+
+    private val allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    private val base = allowedCharacters.length
+
+    fun encode(uniqueId: Long): String {
+        var input = uniqueId
+        val encodedString = StringBuilder()
+        if (input == 0L) {
+            return allowedCharacters[0].toString()
+        }
+        while (input > 0) {
+            encodedString.append(allowedCharacters[(input % base).toInt()])
+            input /= base
+        }
+        return encodedString.reverse().toString()
+    }
 }
